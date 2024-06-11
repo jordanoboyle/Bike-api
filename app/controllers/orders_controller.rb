0cb965b87559d82830d0d29a99297c53587b4421
jordanoboyle <jmcmcode@gmail.com>
2024-06-11 00:40:02 -0600

    order param code dynamic httpie

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,16 @@
 class OrdersController < ApplicationController
 
   def create
-    order = Order.new(
-      user_id: 1,
-      bike_id: 2,
-      quantity: 1,
-      subtotal: 500,
-      tax: 50,
-      total: 550,
+    p current_user
+    @order = Order.new(
+      user_id: params[:user_id], #from current user
+      bike_id: params[:bike_id],
+      quantity: params[:quantity], 
+      subtotal: params[:subtotal], # from bike
+      tax: params[:tax], #need to make q based
+      total: params[:total], #need to make q based
     )
-    if order.save
+    if @order.save
       render template: "orders/show"
     end
   end
